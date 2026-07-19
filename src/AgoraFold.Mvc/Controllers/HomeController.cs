9eb8f8e5851a94608490fbfd99a638ce2dd8cb9c
Dmitry Grigoryev <1461123+sqrt57@users.noreply.github.com>
2026-07-19 18:24:29 +0200

    Make listings the home page, remove scaffolded Home pages
    Default route and navbar brand now point at Listings/Index. Home/Index
    and Home/Privacy (leftover MVC scaffold pages) are removed; HomeController
    keeps only Error and StatusCode, which the pipeline still targets.

--- src/AgoraFold.Mvc/Controllers/HomeController.cs
+++ src/AgoraFold.Mvc/Controllers/HomeController.cs
@@ -6,16 +6,6 @@ namespace AgoraFold.Mvc.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- src/AgoraFold.Mvc/Program.cs
+++ src/AgoraFold.Mvc/Program.cs
@@ -46,7 +46,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Listings}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
